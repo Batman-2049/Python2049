d479c722d5c17e815852ee34202519fb024ae57b
DESKTOP-9PIGATV\Peace <ut@gc.com>
2024-09-23 15:17:11 -0500

    #intial test

---
+++ main.py
@@ -0,0 +1,17 @@
+# Square Area Prompt - Peace Ocran
+print('Enter the lenght of the side:')
+side = int(input())
+
+print('Your area is', side * side)
+print('Your circumference is', 4 * side)
+
+
+
+# Salary Prompt - Peace Ocran
+print('Please enter your annual salary:')
+userAnnualSalary = int(input())
+userBonus = userAnnualSalary * .10
+
+print('Congrats ! You received a bonus of', userBonus)
+print('Your total salary for this year is', userBonus + userAnnualSalary )
+
